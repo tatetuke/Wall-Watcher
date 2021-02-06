d24f53bb630a781e9a9c403e1489ef5c45b8c0d6
watabu <4618085@ed.tus.ac.jp>
2021-02-06 16:41:21 +0900

    タイトルシーンのcommitし直し
    "タイトルを作りました。タイトルからのシーンの切り替えを作りました。0.01"のcommit

---
+++ Assets/Scripts/TitleScripts/TItleUIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TItleUIController : MonoBehaviour
+{
+    [SerializeField]
+    Animator PushAnyKeyAnimator;
+    [SerializeField]
+    Animator TitleLogoAnimator;
+    
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log("何らかのキーが押されました");
+            PushAnyKeyAnimator.SetBool("IsPushAnyKey", true);
+            TitleLogoAnimator.SetBool("IsPushAnyKey", true);
+        }
+    }
+}
