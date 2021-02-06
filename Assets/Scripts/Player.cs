a43e3a9bd15fd26925b36ecf94d6b0d84aac88b8
watabu <4618085@ed.tus.ac.jp>
2021-02-06 18:39:56 +0900

    プレイヤー移動、追従カメラを追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,8 +11,10 @@ public class Player : MonoBehaviour
     private float m_WalkSpeed = 0;
     [SerializeField]
     private State m_State = default;
+    [SerializeField]
+    private GameObject m_ViewObject = default;
 
-    private CharacterController m_CharacterController = default;
+    //private CharacterController m_CharacterController = default;
 
     public enum State
     {
@@ -25,7 +27,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_CharacterController = GetComponent<CharacterController>();
+        m_State = State.IDEL;
+        //m_CharacterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -36,10 +39,22 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        float moveX = Input.GetAxis("Horizontal") * m_WalkSpeed;
+        float moveX = Input.GetAxis("Horizontal") * m_WalkSpeed * Time.deltaTime;
         Vector3 vel = new Vector3(moveX, 0, 0);
-        m_CharacterController.SimpleMove(vel);
         
+        //横移動
+        transform.position = transform.position + vel;
+        //m_CharacterController.SimpleMove(vel);
+        //m_CharacterController.Move(vel);
+
+        //移動する向きに見た目を回転
+        if (moveX != 0)
+        {
+            int dir = moveX > 0 ? 180 : 0;
+            //transform.rotation = Quaternion.Euler(0, dir, 0);
+            m_ViewObject.transform.rotation = Quaternion.Euler(0, dir, 0);
+        }
+
     }
 
     public void ChangeState(State stete)
