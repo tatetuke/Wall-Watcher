e0fd5b5244dc780539761090b6d140a62ba2a993
watabu <4618085@ed.tus.ac.jp>
2021-02-15 00:38:23 +0900

    AudioMixerを追加

---
+++ Assets/Scripts/Audio/BGMManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour, IAudio
+{
+    [SerializeField] private AudioSource m_AudioSource = default;
+
+    public void Stop()
+    {
+        m_AudioSource.Stop();
+
+    }
+
+    public bool IsPlaying()
+    {
+        return m_AudioSource.isPlaying;
+    }
+
+
+}
---
+++ Assets/Scripts/Audio/SEManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEManager : MonoBehaviour, IAudio
+{
+    [SerializeField] private AudioSource m_AudioSource = default;
+    [SerializeField] private List<AudioClip> m_AudioClips = default;
+
+    public enum SEType
+    {
+        FOOTSTEPS,
+        ESCAPE,
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            Play(SEType.ESCAPE);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            Play(SEType.FOOTSTEPS);
+    }
+
+    public void Stop()
+    {
+        m_AudioSource.Stop();
+    }
+
+    public bool IsPlaying()
+    {
+        return m_AudioSource.isPlaying;
+    }
+
+    public void Play(SEType type)
+    {
+        Debug.Log(type + "is playing");
+        if ((int)type >= m_AudioClips.Count)
+            return;
+
+        m_AudioSource.PlayOneShot(m_AudioClips[(int)type]);
+
+        switch (type)
+        {
+            case SEType.FOOTSTEPS:
+                break;
+            case SEType.ESCAPE:
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
