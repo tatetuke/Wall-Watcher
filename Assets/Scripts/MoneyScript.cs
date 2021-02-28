790b04375c928922250b139ba3fa7ce6e0ea8b34
Kyoichi <40378788+kyoichi001@users.noreply.github.com>
2021-02-28 17:59:01 +0900

    backup

---
+++ Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 所持金を管理するクラス
+/// </summary>
+public class MoneyScript : MonoBehaviour
+{
+    [SerializeField] int defaultMoney;//初期値
+    [SerializeField] int maxMoney;//お金の最大値
+    [SerializeField,ReadOnly] int currentMoney;//現在の所持金
+    public int Money//現在の所持金
+    {
+        get
+        {
+            return currentMoney;
+        }
+        set
+        {
+            currentMoney = Mathf.Clamp(value, 0, maxMoney);
+        }
+    }
+    private void Awake()
+    {
+        Money= PropertyLoader.Instance.GetInt("money");
+        GamePropertyManager.Instance.RegisterParam("money", () => Money);
+    }
+    private void OnDestroy()
+    {
+       PropertyLoader.Instance.SetInt("money",Money);
+    }
+
+}
