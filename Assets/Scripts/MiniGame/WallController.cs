b624651f92e7ebdc4124f0ad0aa4937406f03a26
Meerkat39 <4619051@ed.tus.ac.jp>
2021-03-06 16:07:29 +0900

    クリックしたら色を変えるタイルを設置
    とりあえず色々やってみる

---
+++ Assets/Scripts/MiniGame/WallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+    private int ClickNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClickNum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetClickNum() 
+    { 
+        return ClickNum; 
+    }
+
+    public void CountUp()
+    {
+        ClickNum++;
+    }
+}
