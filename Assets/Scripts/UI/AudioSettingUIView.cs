27ece7fc7ad8388336e2cf225e1e30946a411635
watabu <4618085@ed.tus.ac.jp>
2021-03-07 23:14:24 +0900

    音量設定UIを作成

--- Assets/Scripts/UI/AudioSettingUIView.cs
+++ Assets/Scripts/UI/AudioSettingUIView.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class AudioSettingUIView : UIView
 {
+    [SerializeField] private GameObject audioSettingUIObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSettingUIObject = gameObject;
+
+        OnViewShow.AddListener(() =>
+        {
+            audioSettingUIObject.SetActive(true);
+        });
+        OnViewHide.AddListener(() =>
+        {
+            audioSettingUIObject.SetActive(false);
+        });
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            OnViewShow.Invoke();
+        }
+
     }
+
+    public void CloseUI()
+    {
+        OnViewHide.Invoke();
+    }
+
 }
