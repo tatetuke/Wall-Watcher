204e0caf47423a963c114378f66028c437bdd461
Meerkat39 <4619051@ed.tus.ac.jp>
2021-03-08 21:59:36 +0900

    保存

---
+++ Assets/Scripts/MiniGame/Manager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// ミニゲームの内容自体を管理する
+/// </summary>
+public class Manager : MonoBehaviour
+{
+    // 壁の状態
+    int[,] Wall;
+    int Num = 0;
+    GameObject Item;
+
+    public GameObject Item1, Item2;
+
+    public void Start()
+    {
+
+    }
+
+
+    public void Update()
+    {
+        Num = 0;
+        
+        GameObject target;
+        target = GetClickObject();
+        if (target != null) Debug.Log(target.name);
+
+        if (target == null) return;
+        else if (target.name == "Green")
+        {
+            Instantiate(Item1);
+            Item = GameObject.Find("Item1(Clone)").gameObject;
+        }
+        else if (target.name == "Blue")
+        {
+            Item = GameObject.Find("Item1(Clone)");
+            if (Item != null) Destroy(Item);
+
+            Item = GameObject.Find("Item2(Clone)");
+            if (Item == null) Instantiate(Item1);
+            Item = GameObject.Find("Item2(Clone)");
+        }
+        else if (target.name == "White")
+        {
+            Item = GameObject.Find("Item1(Clone)");
+            if (Item != null) Destroy(Item);
+            Item = GameObject.Find("Item2(Clone)");
+            if (Item != null) Destroy(Item);
+            Item = null;
+        }
+        else
+        {
+            Wall wall;
+            wall = target.GetComponent<Wall>();
+            wall.ChangeSprite(Num);
+        }
+
+        if (Item != null)
+        {
+            Debug.Log("更新");
+            Vector3 touchScreenPosition = Input.mousePosition;
+
+            touchScreenPosition.x = Mathf.Clamp(touchScreenPosition.x, 0.0f, Screen.width);
+            touchScreenPosition.y = Mathf.Clamp(touchScreenPosition.y, 0.0f, Screen.height);
+
+            // 10.0fに深い意味は無い。画面に表示したいので適当な値を入れてカメラから離そうとしているだけ.
+            touchScreenPosition.z = 10.0f;
+
+            Camera gameCamera = Camera.main;
+            Vector3 touchWorldPosition = gameCamera.ScreenToWorldPoint(touchScreenPosition);
+
+            Item.transform.position = touchWorldPosition;
+        }
+    }
+
+    
+
+    private GameObject GetClickObject()
+    {
+        GameObject result = null;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Num = +1;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                result = hit.collider.gameObject;
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Num = -1;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                result = hit.collider.gameObject;
+            }
+        }
+        return result;
+    }
+}
---
+++ Assets/Scripts/MiniGame/Wall.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    const int CRACKED = 0;
+    const int NORMAL = 1;
+    const int PAINTED = 2;
+
+    public Sprite[] Sprites;
+
+
+    private int State;
+
+    private int ClickNum;
+    public byte ColorNum;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Random.value <= 0.1f) State = CRACKED;
+        else State = NORMAL;
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = Sprites[State];
+
+        ClickNum = 0;
+        ColorNum = 255;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int GetState()
+    {
+        return State;
+    }
+
+
+
+    public void ChangeSprite(int num)
+    {
+        if (State == CRACKED)
+        {
+            if (num > 0) return;
+            State = NORMAL;
+        }
+        else if (State == NORMAL)
+        {
+            if (num < 0) return;
+            State = PAINTED;
+        }
+        else if (State == PAINTED)
+        {
+            return;
+        }
+
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = Sprites[State];
+    }
+
+    public int GetClickNum()
+    {
+        return ClickNum;
+    }
+
+    public void CountUp()
+    {
+        ClickNum++;
+    }
+
+    public void Change()
+    {
+        this.GetComponent<SpriteRenderer>().color = Color.gray;
+    }
+}
