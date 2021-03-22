7b1b241ffc7383fb93f94c88c36cb011fbf37d59
watabu <4618085@ed.tus.ac.jp>
2021-03-23 00:05:12 +0900

    前準備
    ミニマップ画像の追加、スクリプトの仮組

---
+++ Assets/Scripts/Minimap/Minimap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 画面に表示される階層ごとのマップUI
+/// </summary>
+public class Minimap : MonoBehaviour
+{
+    [SerializeField]private GameObject scenePlaceInfoParent;
+    private Dictionary<string, ScenePlaceInfo> scenePlaceInfoDictionary;
+
+    private void Awake()
+    {
+        //scenePlaceInfoParentの子供のオブジェクトを辞書に追加
+        scenePlaceInfoDictionary.Clear();
+        foreach(Transform childTransform in scenePlaceInfoParent.transform)
+        {
+            var scenePlaceInfo = childTransform.GetComponent<ScenePlaceInfo>();
+            if (scenePlaceInfo != null)
+                scenePlaceInfoDictionary.Add(childTransform.name, scenePlaceInfo);
+        }
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Minimap/ScenePlaceInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーの位置とミニマップ上での位置を紐づける
+/// </summary>
+public class ScenePlaceInfo : MonoBehaviour
+{
+    [Header("ゲームオブジェクト名から自動で取得")]
+    [SerializeField, ReadOnly] private string sceneName;
+
+    [SerializeField] private Transform startPosition;
+    [SerializeField] private Transform endPosition;
+
+    /// <summary>0~1の値からミニマップ上での座標を返す</summary>
+    /// <param name="progress">0~1の値　マップ上での進み具合</param>
+    /// <returns>Minimap上での座標</returns>
+    public Vector3 GetPlayerPosition(float progress)
+    {
+        return Vector3.zero;
+    }
+}
