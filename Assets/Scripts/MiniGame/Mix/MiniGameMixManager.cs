a82b0e5c7cfe087a99d3ce5fe756e6d65d64a0fd
Meerkat39 <4619051@ed.tus.ac.jp>
2021-03-29 23:04:37 +0900

    Mixシーンの追加

--- Assets/Scripts/MiniGame/Mix/MiniGameMixManager.cs
+++ Assets/Scripts/MiniGame/Mix/MiniGameMixManager.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class MiniGameMixManager : MonoBehaviour
 {
 
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (/*左クリックが押されたら*/Input.GetMouseButtonDown(0))
+        {
+            GameObject cursorObject = GetCursorObject();
+            if (cursorObject != null)
+            {
+                if (cursorObject.name == "ww_tyougou_botan1")
+                {
+                    Debug.Log("↑ボタンが押されました");
+                }
+                else if (cursorObject.name == "ww_tyougou_botan2")
+                {
+                    Debug.Log("↓ボタンが押されました");
+                }
+            }
+        }
+    }
 
+    private GameObject GetCursorObject()
+    {
+        GameObject cursorObject;
+        cursorObject = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+        if (hit2d)
+        {
+            cursorObject = hit2d.transform.gameObject;
+        }
+        return cursorObject;
     }
 }
