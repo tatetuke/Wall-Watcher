fc7c69d06d8ba694e8c06d48c494279f4001b7a5
Meerkat39 <4619051@ed.tus.ac.jp>
2021-03-29 16:33:03 +0900

    近づいたら光らせる処理を追加

---
+++ Assets/Scripts/MiniGame/Mix/WalllPollutionManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalllPollutionManager : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SetGlowLine(Color.yellow);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SetGlowLine(Color.cyan);
+        }
+    }
+
+    void SetGlowLine(Color color)
+    {
+        Material material = this.GetComponent<Renderer>().material;
+        material.SetColor("Color_7C7012AB", color);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
