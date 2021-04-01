d864a65eea80863afb1e651e0087b7cf68083b05
Kyoichi <40378788+kyoichi001@users.noreply.github.com>
2021-04-01 18:40:14 +0900

    gamemanager destroy update

---
+++ Assets/Scripts/ManagerDeleter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerDeleter : MonoBehaviour
+{
+    [SerializeField] string winner_name = "Managers_Strong";
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        var gameManagers = FindObjectsOfType<Kyoichi.GameManager>();
+        foreach(var i in gameManagers)
+        {
+            if(i.gameObject.name!= winner_name)
+            {
+                Destroy(i.gameObject);
+            }
+        }
+    }
+}
