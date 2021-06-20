5d148f597c9f194ebea7cc85af51efc876696cfc
Kyoichi <40378788+kyoichi001@users.noreply.github.com>
2021-06-20 20:25:18 +0900

    セーブロード説明用のテストスクリプトを作成

---
+++ Assets/Scenes/ItemTest/DebugItemCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ItemTest
+{
+    public class DebugItemCanvas : UIView
+    {
+        [SerializeField] Transform containerParent;
+        [SerializeField] GameObject containerPrefab;
+        public void LoadItems()
+        {
+            if (Kyoichi.GameManager.Instance.IsLoadFinished)
+            {
+                foreach (var i in Kyoichi.ItemManager.Instance.Data)
+                {
+                    var scr = Instantiate(containerPrefab, containerParent).GetComponent<DebugItemContainerScript>();
+                    scr.init(i.Value.item_name, "0");
+                }
+            }
+        }
+    }
+}
