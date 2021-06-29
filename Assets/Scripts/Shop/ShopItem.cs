0ceb7decee3deddc552c7be5cb95a0a74ec07e9e
tatetuke <4619094@ed.tus.ac.jp>
2021-06-29 14:41:57 +0900

    購入アイテム，アイテム選択によるUIの変更，スクリプタブルオブジェクト等

---
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class ShopItem : MonoBehaviour
+{
+    public ItemSO itemdata;
+    GameObject descriptionObj;
+    GameObject UIItemNameObj;
+    GameObject IconObj;
+    TextMeshProUGUI description;
+    TextMeshProUGUI UIItemName;
+    Image Icon;
+    Text ItemName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ItemName = this.gameObject.transform.GetChild(0).GetComponent<Text>();
+        descriptionObj = GameObject.Find("ShopUIItemDescription");
+        IconObj = GameObject.Find("ShopUIItemIcon");
+        UIItemNameObj = GameObject.Find("ShopUIItemName");
+        Icon = IconObj.GetComponent<Image>();
+        description = descriptionObj.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        Debug.Log("アイテム生成");
+        UIItemName = UIItemNameObj.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        if (itemdata == null) Debug.Log("error");
+        if (description == null) Debug.Log("error1");
+        ItemName.text = itemdata.item_name;
+    }
+
+    /// <summary>
+    ///UIのテキスト，アイコンを変更する関数．
+    /// </summary>
+    public void ChangeUI()
+    {
+        description.text = itemdata.description;
+        Icon.sprite = itemdata.icon;
+        UIItemName.text = itemdata.item_name;
+    }
+}
--- Assets/Scripts/Shop/ShopItemGenerater.cs
+++ Assets/Scripts/Shop/ShopItemGenerater.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class ShopItemGenerater : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ShopLineUpSO ShopLineup;
+    List<ItemSO> Item = new List<ItemSO>();
 
+    private void Start()
+    {
+        Item = ShopLineup.GetItemLists();
+        PrepareItem();
+       
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// ショップアイテムのリストにプレハブを生成．
+    /// </summary>
+    private void PrepareItem()
+    {
+        GameObject prefab = (GameObject)Resources.Load("Shop/ShopItem");
+        for (int i = 0; i < Item.Count; i++)
+        {
+            GameObject instance=(GameObject)Instantiate(prefab,new Vector3(0f,0f,0f),Quaternion.identity);
+            instance.transform.parent = this.transform;
+            instance.GetComponent<ShopItem>().itemdata = Item[i];
+            //instance.GetComponent<ShopItem>().ItemName.text = Item[i].item_name;
+        }
+            Debug.Log("生成終わり？");
+
     }
 }
