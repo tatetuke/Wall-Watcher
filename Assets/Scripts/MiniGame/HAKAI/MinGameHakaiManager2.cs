cbc3e1e4d46a1b00b8ce660c3aab6f6da34fb268
tatetuke <4619094@ed.tus.ac.jp>
2021-07-29 16:34:16 +0900

    ミニゲーム．ダイヤモンドパールのやつ

--- Assets/Scripts/MiniGame/HAKAI/MinGameHakaiManager2.cs
+++ Assets/Scripts/MiniGame/HAKAI/MinGameHakaiManager2.cs
@@ -15,8 +15,8 @@ public class MinGameHakaiManager2 : MonoBehaviour
     int[] dx = new int[9] { -1, 0, 1, -1, 0, 1, -1, 0, 1 };//裏返す壁のIndex
     int[] dy = new int[9] { -1, -1, -1, 0, 0, 0, 1, 1, 1 };//
     private string PolutedLevel1;//壁の画像の名前
-    private string PolutedLevel1_1;//
-    public string PolutedLevel2;//
+    private string PolutedLevel2;//
+    public string PolutedLevel3;//
     [SerializeField] private UnityEvent UpdateItemData=new UnityEvent(); //アイテムデータのアップデート
 
     [SerializeField] private MinGameHAKAIStatus gameStatus;//HPやHPを減らす関数を持つクラス
@@ -271,7 +271,7 @@ public class MinGameHakaiManager2 : MonoBehaviour
             m_Wall.GetComponent<SpriteRenderer>().sprite = WallSprite[2];
             animt.SetTrigger("StartAnime");
         }
-        else if (spriteName == PolutedLevel2)
+        else if (spriteName == PolutedLevel3)
         {
             m_Wall.GetComponent<SpriteRenderer>().sprite = WallSprite[0];
             animt.SetTrigger("StartAnime");
@@ -290,18 +290,16 @@ public class MinGameHakaiManager2 : MonoBehaviour
         if (spriteName == PolutedLevel1) {
             m_Wall.GetComponent<SpriteRenderer>().sprite = WallSprite[1];
         }
-        else if(spriteName==PolutedLevel1_1)
+        else if(spriteName==PolutedLevel2)
         {
             m_Wall.GetComponent<SpriteRenderer>().sprite = WallSprite[2];
             
-        }else if (spriteName == PolutedLevel2)
+        }else if (spriteName == PolutedLevel3)
         {
-
-            m_Wall.GetComponent<SpriteRenderer>().sprite = WallSprite[0];
+            return;
+            //m_Wall.GetComponent<SpriteRenderer>().sprite = WallSprite[0];//サイクルバージョン
 
         }
-        
-
     }
     /// <summary>
     /// DPバージョン
@@ -314,7 +312,7 @@ public class MinGameHakaiManager2 : MonoBehaviour
         {
             m_Wall.GetComponent<SpriteRenderer>().sprite = WallSprite[1];
         }
-        else if (spriteName == PolutedLevel1_1)
+        else if (spriteName == PolutedLevel2)
         {
             m_Wall.GetComponent<SpriteRenderer>().sprite = WallSprite[2];
 
@@ -326,8 +324,8 @@ public class MinGameHakaiManager2 : MonoBehaviour
     private void GetSpriteName()
     {
         PolutedLevel1 = WallSprite[0].name;
-        PolutedLevel1_1 = WallSprite[1].name;
-        PolutedLevel2 = WallSprite[2].name;
+        PolutedLevel2 = WallSprite[1].name;
+        PolutedLevel3 = WallSprite[2].name;
 
 
     }
