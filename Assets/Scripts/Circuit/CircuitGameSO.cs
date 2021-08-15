874797b687fa003dcb83e52ccde0a4cff879eda0
Kyoichi <40378788+kyoichi001@users.noreply.github.com>
2021-08-16 02:38:48 +0900

    ファイル移動

---
+++ Assets/Scripts/Circuit/CircuitGameSO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 機械の修理ゲームを定義するScriptableObject
+/// </summary>
+[CreateAssetMenu(fileName = "NewCircuitGame", menuName = "ScriptableObject/CircuitGame")]
+public class CircuitGameSO : ScriptableObject
+{
+    //この辺は使わないかも
+    [Tooltip("修理クエストの名前")]
+    public string questName;
+    [Tooltip("修理クエストの説明")]
+    public string description;
+
+    [Tooltip("修理に必要なパーツ")]
+    public List<CircuitSO> requiredCircuit = new List<CircuitSO>();
+
+    
+
+}
--- Assets/Scripts/Circuit/CircuitSO.cs
+++ Assets/Scripts/Circuit/CircuitSO.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 回路パーツを定義するScriptableObject
+/// </summary>
 [CreateAssetMenu(fileName = "NewCircuit", menuName = "ScriptableObject/Circuit")]
 public class CircuitSO : ItemSO
 {
