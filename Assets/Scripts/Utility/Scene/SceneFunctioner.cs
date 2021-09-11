0c3efa583dfee2aabdbab1e907511cc512986ed9
Kyoichi <40378788+kyoichi001@users.noreply.github.com>
2021-09-11 17:15:35 +0900

    SceneChangeWrapperの修正。SingletonGeneratorの作成

---
+++ Assets/Scripts/Utility/Scene/SceneFunctioner.cs
@@ -0,0 +1,21 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFunctioner : MonoBehaviour
+{
+
+
+
+
+    public async UniTask SceneStartAsync(Kyoichi.Context context)
+    {
+
+    }
+
+    public async UniTask SceneEndAsync()
+    {
+
+    }
+}
