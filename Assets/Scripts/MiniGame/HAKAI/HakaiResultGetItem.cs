616bfdf7ba03da3ad944d894307dc172a3f663a9
tatetuke <4619094@ed.tus.ac.jp>
2022-02-24 15:33:22 +0900

    画面修正、アニメーションの追加

---
+++ Assets/Scripts/MiniGame/HAKAI/HakaiResultGetItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HakaiResultGetItem : MonoBehaviour
+{
+    public GameObject itemIcon;
+    public GameObject itemDiscription;
+    public GameObject itemInInventoryNum;
+    public GameObject itemGetNum;
+    public GameObject itemName;
+    public Animator animator;
+
+
+    public bool endFadeInAnime=false;
+
+    public void IsOnEndFadeInAnime()
+    {
+        endFadeInAnime = true;
+    }
+
+}
