149c2d59435c07532d14d68e158532a7be4fe457
anman6347 <4620096@ed.tus.ac.jp>
2022-03-04 21:13:55 +0900

    create MainRoom1,2,3

--- Assets/Scripts/MainMap/MoveEvent/mapEnum.cs
+++ Assets/Scripts/MainMap/MoveEvent/mapEnum.cs
@@ -1,10 +1,19 @@
-﻿
+﻿/*
+ * マップ移動に関する列挙体のリスト
+ * 
+ * 
+ */
+
+
 namespace map {
     // マップ番号
     public enum MAP_NUM
     {
         MainMap2F_Floor,
         MainMap3F_Floor,
+        MainMap2F_MainRoom1,
+        MainMap2F_MainRoom2,
+        MainMap2F_MainRoom3,
         MainMap3_Room1,
         MainMap3_Room2,
         MainMap3F_Renraku1,
